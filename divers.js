e6a42b52b6cb3396121089fb50e6ea69c8626229
Lancelot Watelle <lancelotwatelle@gmail.com>
2024-03-09 22:56:31 +0100

    End2dRendu

--- divers.js
+++ divers.js
@@ -16,3 +16,111 @@ function Nouveau_mot(){
     return word_to_return;
 }
 
+
+//Citation
+let yes = document.getElementById("IWant");
+let no = document.getElementById("noThanks");
+
+function putQuote(event) {
+    let citation = document.getElementById("theQuote");
+    citation.innerHTML = Nouveau_mot();
+    citation.style.display = "contents";
+}
+
+function RemoveQuote() {
+    let citation = document.getElementById("theQuote");
+    citation.style.display = "none";
+}
+
+yes.addEventListener("click", putQuote)
+no.addEventListener("click", RemoveQuote);
+
+//Detail skills
+let bouton_hard = document.getElementsByClassName("def")[0];
+let Hard_def = document.getElementById("whatIsHardSkills");
+let Hard_activ = false;
+let bouton_soft = document.getElementsByClassName("def")[1];
+let Soft_def = document.getElementById("whatIsSoftSkills");
+let Soft_activ = false;
+
+function putHard(event) {
+    if(Hard_activ){
+        Hard_def.style.display = "none";
+        Hard_activ = false;
+} else {
+    Hard_activ = true;
+    Hard_def.style.display = "contents";
+}
+}
+
+function putSoft(event) {
+    if(Soft_activ){
+        Soft_def.style.display = "none";
+        Soft_activ = false;
+} else {
+    Soft_activ = true;
+    Soft_def.style.display = "contents";
+}
+}
+
+bouton_hard.addEventListener("click", putHard);
+bouton_soft.addEventListener("click", putSoft);
+
+
+//Explanation for projects
+let ArrayButton = document.getElementsByClassName("explainProject");
+let ArrayContent = document.getElementsByClassName("Theexplaination");
+var ArrayActivation = [false, false, false];
+console.log(ArrayButton);
+console.log(ArrayContent);
+
+console.log(ArrayActivation);
+
+
+function Put(number) {
+    console.log(2);
+    if(!ArrayActivation[number]) {
+        console.log(1);
+        ArrayContent[number].style.display = "contents";
+        ArrayActivation[number] = true;
+    } else {
+        console.log(2); 
+        ArrayContent[number].style.display = "none";
+        ArrayActivation[number] = true;
+    }
+}
+
+ArrayButton[0].addEventListener("click", function() {
+    Put(0)
+});
+ArrayButton[1].addEventListener("click", function() {
+    Put(1)
+});
+ArrayButton[2].addEventListener("click", function() {
+    Put(2)
+});
+
+//footer
+let EmailText = document.getElementById("mymail");
+let EmailPhoto = document.getElementsByClassName("contactMe")[2];
+let Email_activ = false;
+let email_button = EmailPhoto;
+
+function pictureEmail(){
+    if(Email_activ) {
+        console.log(Email_activ);
+        EmailText.style.display = "none";
+        Email_activ = false;
+    } else {
+        console.log(Email_activ);
+        EmailText.style.display = "contents";
+        Email_activ = true;
+    }
+}
+
+email_button.addEventListener("click",pictureEmail)
+
+
+
+
+
